0934610528ab22ee31bf1e6d528cb6a6f70437e4
prl <1461881720@qq.com>
2025-11-06 18:06:47 +0800

    feat: initial react

---
+++ my-react-app/src/pages/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function HomePage() {
+
+  return (
+    <>
+      <h1>New Entrance</h1>
+    </>
+  )
+}
+
+export default HomePage
---
+++ my-react-app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  corePlugins: {
+    preflight: false, // disable css reset
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
