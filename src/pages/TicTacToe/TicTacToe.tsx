631804ffee543282ef506e763b8f782b16b7ac30
prl <1461881720@qq.com>
2025-11-12 17:52:23 +0800

    fix: lifting tictactoe state up

--- src/pages/TicTacToe/TicTacToe.tsx
+++ src/pages/TicTacToe/TicTacToe.tsx
@@ -17,7 +17,7 @@ function Square({
   );
 }
 
-function TicTacToe() {
+function Board() {
   const [isXNext, setIsXNext] = useState(true);
   const [squares, setSquare] = useState(Array(9).fill(null));
   const Winner = calculateWinner(squares);
@@ -27,6 +27,7 @@ function TicTacToe() {
   } else {
     status = `Next player: ${isXNext ? "X" : "O"}`;
   }
+
   function handleClick(index: number) {
     // direct reception parameters
     const nextSquares = squares.slice();
@@ -66,23 +67,35 @@ function TicTacToe() {
 
   return (
     <>
+      <div>
+        <div>{status}</div>
+        <div>
+          <div className="flex">
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+          </div>
+          <div className="flex">
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+          </div>
+          <div className="flex">
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function TicTacToe() {
+  return (
+    <>
       <Nav />
-      <div>{status}</div>
-      <div className="flex">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="flex">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="flex">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      <Board />
     </>
   );
 }
